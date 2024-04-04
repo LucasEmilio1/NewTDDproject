4b2146436d295a2d8c6a359282d2029c82e43cee
LucasEmilio1 <lucasf.emilio@gmail.com>
2024-04-03 21:21:45 -0300

    comentario extra

--- TrabalhoTrio/Calcula.cs
+++ TrabalhoTrio/Calcula.cs
@@ -8,6 +8,12 @@ namespace TrabalhoTrio
 {
     public class Calcula
     {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="itemA"></param>
+        /// <param name="itemB"></param>
+        /// <returns></returns>
         public int Soma(int itemA, int itemB)
         {
             return itemA + itemB;
